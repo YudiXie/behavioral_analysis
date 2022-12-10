835be674675374fe33e49390f37611ccdfe96c57
yudixie <yudixie235@gmail.com>
2022-12-10 09:49:27 -0500

    add bar to indicate statistical significance

--- plots.py
+++ plots.py
@@ -109,6 +109,70 @@ def plot_trajectories(exp_name,
     plt.close()
 
 
+def barplot_annotate_brackets(idx1, idx2, data, center, height, yerr=None, dh=.05, barh=.05, fs=None, maxasterix=None):
+    """
+    Annotate barplot with p-values.
+    adapted from:
+    https://stackoverflow.com/questions/11517986/indicating-the-statistically-significant-difference-in-bar-graph
+
+    :param idx1: index of left bar to put bracket over
+    :param idx2: index of right bar to put bracket over
+    :param data: string to write or number for generating asterixes
+    :param center: centers of all bars (like plt.bar() input)
+    :param height: heights of all bars (like plt.bar() input)
+    :param yerr: yerrs of all bars (like plt.bar() input)
+    :param dh: height offset over bar / bar + yerr in axes coordinates (0 to 1)
+    :param barh: bar height in axes coordinates (0 to 1)
+    :param fs: font size
+    :param maxasterix: maximum number of asterixes to write (for very small p-values)
+    """
+
+    if type(data) is str:
+        text = data
+    else:
+        # * is p < 0.05
+        # ** is p < 0.005
+        # *** is p < 0.0005
+        # etc.
+        text = ''
+        p = .05
+
+        while data < p:
+            text += '*'
+            p /= 10.
+
+            if maxasterix and len(text) == maxasterix:
+                break
+
+        if len(text) == 0:
+            text = 'n. s.'
+
+    lx, ly = center[idx1], height[idx1]
+    rx, ry = center[idx2], height[idx2]
+
+    if yerr:
+        ly += yerr[idx1]
+        ry += yerr[idx2]
+
+    ax_y0, ax_y1 = plt.gca().get_ylim()
+    dh *= (ax_y1 - ax_y0)
+    barh *= (ax_y1 - ax_y0)
+
+    y = max(ly, ry) + dh
+
+    barx = [lx, lx, rx, rx]
+    bary = [y, y+barh, y+barh, y]
+    mid = ((lx+rx)/2, y+barh)
+
+    plt.plot(barx, bary, c='black')
+
+    kwargs = dict(ha='center', va='bottom')
+    if fs is not None:
+        kwargs['fontsize'] = fs
+
+    plt.text(*mid, text, **kwargs)
+
+
 def two_set_scatter_plot(data1, data2, labels, title_str, ylabel, save_str):
     """
     scatter plot for two groups
@@ -158,6 +222,10 @@ def two_set_scatter_plot(data1, data2, labels, title_str, ylabel, save_str):
     plt.xlim([0.5, 2.5])
     plt.title(title_str + "\nMann-Whitney U rank test P = {:.3f}".format(p_value))
     plt.ylabel(ylabel)
+    barplot_annotate_brackets(0, 1, p_value, [1, 2],
+                              [mean_data1, mean_data2],
+                              [data1.max()-mean_data1, data2.max()-mean_data1],
+                              barh=0.025)
     adjust_figure()
     plt.savefig(os.path.join('./figures/', save_str + '.pdf'), transparent=True, bbox_inches="tight")
     plt.close()
